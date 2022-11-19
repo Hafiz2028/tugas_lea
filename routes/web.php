8c7d89116b9ce40270fdf6112c9135fad992dbcc
Hafiz2028 <91659690+Hafiz2028@users.noreply.github.com>
2022-11-19 23:20:26 +0700

    loginsucces

--- routes/web.php
+++ routes/web.php
@@ -22,3 +22,8 @@ use Illuminate\Support\Facades\Route;
 Auth::routes();
 Route::get('/',[App\Http\Controllers\HomeController::class, 'index'])->name('home');
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/logout', function(){
+    \Auth::logout();
+    return redirect('/home');
+});
